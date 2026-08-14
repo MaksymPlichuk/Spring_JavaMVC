30fbcd3b029a647e5847d459df0617a1c1c0ecd4
MaksymPlichuk <pilipm449@gmail.com>
2026-08-14 15:51:58 +0300

    First page

---
+++ SpringAPI/src/main/java/org/example/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") //Шлях до головної сторінки автоматично шукає файл з таким іменем у templates
+    public String index(){
+        return "index";
+    }
+}
